81aed9a6f38be7dea81b93c89dcdaaf2d5d08abc
Robert Gleason <l2ob122@gmail.com>
2024-04-02 21:15:06 -0700

    Add ownership and borrowing examples

--- ownership/references_borrowing/src/main.rs
+++ ownership/references_borrowing/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("Epiq");
+    let len = calculate_length(&s1);
+    println!("The length of {s1} is {len}");
+
+    // mutable reference
+    let mut s = String::from("Edmonton");
+    change(&mut s);
+    println!("{s}");
+
+    let s1 = String::from("Epiq");
+    let r1 = &s1;
+    let r2 = &s1;
+    println!("{r1}, {r2}");
+    // r1 abd r2 will not be used after this point
+}
+
+fn calculate_length(s: &String) -> usize {
+    // s is a refrence to a string
+    s.len()
+} // s goes out of scope. But it does not have ownership of what it referes to, it is not dropped
+
+// Mutable Reference
+fn change(some_string: &mut String) {
+    some_string.push_str(" Oilers");
 }
